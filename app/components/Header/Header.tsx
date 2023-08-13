289e2aff0881b909d3b87ba3d4ab65e3ac5a3f43
BijanProgrammer <bijaneisapour@gmail.com>
2023-08-14 00:36:37 +0330

    [ADD] hero section

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -15,10 +15,6 @@ export default function Header(): ReactElement {
 
     return (
         <header className={'page-bleed ' + styles.header}>
-            <svg className={styles.blob}>
-                <path d="M0,367.99C70.563,374.027,145.498,356.59,200.473,311.943C253.449,268.92,266.471,196.502,290.028,132.451C311.79,73.281,331.723,15.187,332.8,-47.849C334.034,-120.075,349.168,-207.443,296.681,-257.075C243.527,-307.338,152.614,-260.306,83.32,-283.761C9.297,-308.817,-38.747,-404.243,-116.555,-396.951C-193.103,-389.777,-248.819,-315.53,-287.381,-249.017C-322.468,-188.499,-312.3,-115.539,-320.02,-46.012C-327.452,20.915,-358.543,89.73,-331.862,151.557C-305.056,213.674,-236.246,242.472,-179.578,279.429C-122.518,316.642,-67.874,362.183,0,367.99"></path>
-            </svg>
-
             <Link href="/" className={`${styles.logo} + ${acme.className}`}>
                 Schort
             </Link>
@@ -50,7 +46,7 @@ export default function Header(): ReactElement {
             </nav>
 
             <div className={styles.auth}>
-                <Link href="/" className={styles['call-to-action']}>
+                <Link href="/auth" className={styles['call-to-action']}>
                     Sign Up for FREE
                 </Link>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,51 @@
 import styles from './page.module.scss';
 import {ReactElement} from 'react';
 import {Acme} from 'next/font/google';
+import Image from 'next/image';
+
+import heroIllustration from './assets/illustrations/hero.svg';
+import Link from 'next/link';
 
 const acme = Acme({weight: '400', subsets: ['latin']});
 
 export default function Home(): ReactElement {
     return (
         <div className={styles['home-page']}>
-            <svg className={styles.blob}>
-                <path d="M1440 997.28C1526.783 1007.1990000000001 1616.831 996.427 1693.011 953.692 1771.632 909.588 1830.612 837.469 1869.694 756.235 1909.2939999999999 673.925 1933.897 581.09 1915.296 491.663 1897.173 404.534 1835.5819999999999 334.09299999999996 1769.49 274.496 1708.964 219.918 1634.1399999999999 187.99900000000002 1555.365 167.10199999999998 1478.284 146.654 1397.402 131.901 1321.254 155.58800000000002 1245.694 179.09199999999998 1194.542 243.164 1136.8229999999999 297.295 1072.1779999999999 357.922 986.711 406.003 963.142 491.438 938.804 579.66 961.2570000000001 678.952 1010.202 756.282 1056.6979999999999 829.742 1145.786 857.2909999999999 1221.617 899.8109999999999 1292.586 939.605 1359.162 988.04 1440 997.28"></path>
-            </svg>
-
             <div className={styles.hero}>
-                <h1>
-                    <div className={styles.subtitle}>
-                        make it <span className={styles.schort}>schort</span>
+                <div>
+                    <h1>
+                        <div className={styles.subtitle}>
+                            make it <span className={styles.schort}>schort</span>
+                        </div>
+                        <div className={`${styles.title} ${acme.className}`}>
+                            <span className={styles['size-matters']}>size matters!</span>
+                        </div>
+                    </h1>
+
+                    <p className={styles.description}>
+                        We can help you generate a short link from any URL. Short links are easier to share in social
+                        medias and they are also more likely to be remembered.
+                    </p>
+
+                    <div className={styles['input-box']}>
+                        <input type="text" placeholder="Put your link here..." />
+                        <button className={acme.className}>Generate</button>
                     </div>
-                    <div className={`${styles.title} ${acme.className}`}>
-                        <span className={styles['size-matters']}>size matters!</span>
-                    </div>
-                </h1>
+
+                    <p className={styles['suggestion']}>
+                        <strong>But Wait!</strong> There is an even better option... Sign up and see the links that you
+                        have been generates. You can even edit them or remove them entirely.{' '}
+                        <Link href="/auth" className={styles['call-to-action']}>
+                            Sign up for FREE
+                        </Link>
+                    </p>
+                </div>
+
+                <Image
+                    src={heroIllustration}
+                    alt="an illustration of a middleman that is sitting between searched url and database"
+                    width={1000}
+                />
             </div>
         </div>
     );
