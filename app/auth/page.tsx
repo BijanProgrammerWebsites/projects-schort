9287c64d78a563563b4d2942b5c0dc49f1f86026
BijanProgrammer <bijaneisapour@gmail.com>
2023-08-19 19:44:03 +0330

    [UPDATE] redirect to home if already logged in

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,7 +1,18 @@
 import {ReactElement} from 'react';
 
+import {redirect} from 'next/navigation';
+
+import {getServerSession} from 'next-auth/next';
+import {nextAuthOptions} from '@/app/api/auth/[...nextauth]/options';
+
 import AuthFormComponent from '@/app/auth/form/auth-form.component';
 
-export default function AuthPage(): ReactElement {
+export default async function AuthPage(): Promise<ReactElement> {
+    const session = await getServerSession(nextAuthOptions);
+
+    if (session?.user?.email) {
+        redirect('/');
+    }
+
     return <AuthFormComponent />;
 }
